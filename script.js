ed8147a1b28c5efde5bb6a56cd040f3536f453d5
Njoroge Mungai <njorogemungai1980@gmail.com>
2023-01-06 16:36:00 +0300

    updated narrative in index.html

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.getElementById('mybutton').onclick()  = function() {myfunction()}
+    
+function myfunction () { 
+    document.getElementById("mybutton").innerHTML = "YOU CLICKED ME!";
+}
